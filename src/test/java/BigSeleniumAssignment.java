ebf650cfa37e0544c43136d3d66ff975d7a8f251
Juhász László <rs43tu@INF.ELTE.HU>
2024-05-13 11:16:28 +0200

    Initial commit

---
+++ src/test/java/BigSeleniumAssignment.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class BigSeleniumAssignment {
+
+}
